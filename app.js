ce77fb4d6382f3b7a36ed45506d9ec30166f7e57
germansiriani <conexion@germansiriani.com.ar>
2022-03-30 20:20:28 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+
+//Mi servidor
+const app = express();
+
+const PORT = 3000;
+
+//Se definen vincular carpeta completa de CSS e imagenes etc.
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+  console.log("Servidor activado, Ctrl + C para apagar.");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
+});
